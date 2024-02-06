73a7c5424d8e9e04600df5d0213a348398a599cd
Aishwarya Murade <87976092+AishwaryaMurade@users.noreply.github.com>
2024-02-06 12:23:57 +0530

    code formatting

--- example/src/App.js
+++ example/src/App.js
@@ -1,27 +1,29 @@
 import React from 'react'
-import {  GradientMaker } from 'react-gradient-palette'
+import { GradientMaker } from 'react-gradient-palette'
 
 
 const App = () => {
-  const setGradient =(value)=>{
-document.getElementsByTagName('body')[0].style.backgroundImage = value
+  const setGradient = (value) => {
+    document.getElementsByTagName('body')[0].style.backgroundImage = value
   }
   return (
-  <div style={{display: 'flex',
-    flexWrap: 'wrap',
-    gap: '45px',
-    alignItems: 'center',
-    marginLeft: '50px',
-    marginTop: '40px'}}>
-    <div>
-    <GradientMaker setGradient={setGradient} defaultGradient={['#ff5733', '#d147a3']} />
+    <div style={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      gap: '45px',
+      alignItems: 'center',
+      marginLeft: '50px',
+      marginTop: '40px'
+    }}>
+      <div>
+        <GradientMaker setGradient={setGradient} defaultGradient={['#ff5733', '#d147a3']} />
+      </div>
+
+      <div>
+        <GradientMaker showRadial={true} showConic={true} showCloseBtn={true} setGradient={setGradient} defaultGradient={['#ff5733', '#d147a3']} />
+      </div>
+
     </div>
-
- <div>
- <GradientMaker showRadial={true} showConic={true}  showCloseBtn={true} setGradient={setGradient} defaultGradient={['#ff5733', '#d147a3']} />
- </div>
-
-  </div>
   )
 }
 
--- src/GradientMaker/GradientMaker.js
+++ src/GradientMaker/GradientMaker.js
@@ -7,24 +7,22 @@ import { HiPlusCircle } from "react-icons/hi";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import { RxCross2 } from "react-icons/rx";
 
-export const GradientMaker = ({ setGradient, defaultGradient, showRadial,showConic,onCloseClick, showCloseBtn, className }) => {
+export const GradientMaker = ({ setGradient, defaultGradient, showRadial, showConic, onCloseClick, showCloseBtn, className }) => {
   const [activeTab, setActiveTab] = useState(1);
   var radialVal = showRadial ? showRadial : false
   var conicVal = showConic ? showConic : false
-  var grad = defaultGradient ? defaultGradient.constructor === Array ?  defaultGradient : ['#4ec7d9','#135fca']: ['#4ec7d9','#135fca']
+  var grad = defaultGradient ? defaultGradient.constructor === Array ? defaultGradient : ['#4ec7d9', '#135fca'] : ['#4ec7d9', '#135fca']
   const [colorOne, setColorOne] = useState(grad)
-  // const [colorSecond, setColorSecond] = useState('#135fca')
-
   const [selected, setSelected] = useState(colorOne[0])
   const [selectedInput, setSelectedInput] = useState(1)
   const [direction, setDirection] = useState('')
-  const [output,setOutput] = useState()
+  const [output, setOutput] = useState()
   const [hovered, setHovered] = useState(false);
 
   const styles = {
     tabscontainer: {
       display: 'flex',
-      position:'relative',
+      position: 'relative',
       flexDirection: 'column',
       width: '245px',
       margin: '20px auto',
@@ -46,7 +44,7 @@ export const GradientMaker = ({ setGradient, defaultGradient, showRadial,showCon
       cursor: 'pointer',
       transition: 'background-color 0.3s ease',
       fontWeight: '500',
-      color:'#000'
+      color: '#000'
     },
     tabHover: {
       backgroundColor: '#f0f0f0',
@@ -80,9 +78,9 @@ export const GradientMaker = ({ setGradient, defaultGradient, showRadial,showCon
     inputSection: {
       display: 'flex',
       justifyContent: 'space-between',
-    marginBottom: '5px',
-    height: '32px',
-    boxSizing: 'content-box'
+      marginBottom: '5px',
+      height: '32px',
+      boxSizing: 'content-box'
     },
     inputSectionSpanSvg: {
       fontSize: '28px',
@@ -113,106 +111,112 @@ export const GradientMaker = ({ setGradient, defaultGradient, showRadial,showCon
       gap: '5px',
     },
     selectedSvg: {
-       fontSize: '25px',
+      fontSize: '25px',
       background: '#366495',
       color: '#fff',
       cursor: 'pointer',
       borderRadius: '15px',
     },
-    closeContainer:{
-    position:'absolute',
-    width:'30px',
-    height:'30px',
-    background:'#000000',
-    color: '#fff',
-    borderRadius: '14px',
-    right: '-10px',
-    top: '-10px',
-    cursor:'pointer'
+    closeContainer: {
+      position: 'absolute',
+      width: '30px',
+      height: '30px',
+      background: '#000000',
+      color: '#fff',
+      borderRadius: '14px',
+      right: '-10px',
+      top: '-10px',
+      cursor: 'pointer'
     },
-    closeContainericon:{
-      display:'flex',
-      alignItems:'center',
-      justifyContent:'center',
+    closeContainericon: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
       fontSize: '20px',
       marginTop: '4px'
 
+    },
+    outerDivStyle: {
+      height: "25px",
+      width: "25px",
+      borderRadius: "6px",
+      cursor: "pointer"
     }
   };
 
   const handleTabClick = (tabNumber) => {
 
-      setDirection('')
+    setDirection('')
 
     setActiveTab(tabNumber);
   };
 
   const selectedColor = (color) => {
-     setSelected('#'+color.hex)
+    setSelected('#' + color.hex)
   }
 
-useEffect(()=>{
+  useEffect(() => {
 
-   const newIds = colorOne.slice() //copy the array
-   newIds[selectedInput-1] =selected //execute the manipulations
-   setColorOne(newIds) //set the new state
-},[selectedInput,selected])
+    const newIds = colorOne.slice() //copy the array
+    newIds[selectedInput - 1] = selected //execute the manipulations
+    setColorOne(newIds) //set the new state
+  }, [selectedInput, selected])
 
-const deleteColor = ()=> {
+  const deleteColor = () => {
 
-     colorOne.splice(selectedInput-1,1)
+    colorOne.splice(selectedInput - 1, 1)
 
     setColorOne([...colorOne]);
     setTimeout(() => {
       setSelectedInput(1)
       setSelected(colorOne[0])
     }, 50);
-}
+  }
 
-useEffect(()=>{
-  const directionString = direction ? `${direction}, ` : '';
-   if(activeTab===1){
+  useEffect(() => {
+    const directionString = direction ? `${direction}, ` : '';
+    if (activeTab === 1) {
       setGradient(`linear-gradient(${directionString}${colorOne.join(', ')})`)
       setOutput(`linear-gradient(${directionString}${colorOne.join(', ')})`)
-   }
-   if(activeTab===2){
-    setGradient(`radial-gradient(${directionString}${colorOne.join(', ')})`)
-    setOutput(`radial-gradient(${directionString}${colorOne.join(', ')})`)
-   }
-   if(activeTab===3){
-    setGradient(`conic-gradient(${directionString}${colorOne.join(', ')})`)
+    }
+    if (activeTab === 2) {
+      setGradient(`radial-gradient(${directionString}${colorOne.join(', ')})`)
+      setOutput(`radial-gradient(${directionString}${colorOne.join(', ')})`)
+    }
+    if (activeTab === 3) {
+      setGradient(`conic-gradient(${directionString}${colorOne.join(', ')})`)
       setOutput(`conic-gradient(${directionString}${colorOne.join(', ')})`)
-  }
-},[colorOne,direction,activeTab])
+    }
+  }, [colorOne, direction, activeTab])
 
 
   return (
     <div style={styles.tabscontainer} className={className ? className : 'pickerWrapper'}>
-     {showCloseBtn && <div style={styles.closeContainer} onClick={onCloseClick}><span style={styles.closeContainericon}><RxCross2/></span></div>}
+      {showCloseBtn && <div style={styles.closeContainer} onClick={onCloseClick}><span style={styles.closeContainericon}><RxCross2 /></span></div>}
       <div style={styles.tabs}>
-       {(radialVal || conicVal) && <div
-        onMouseEnter={() => setHovered(1)}
-        onMouseDown={() => setHovered(false)}
-        onMouseLeave={() => setHovered(false)}
-          style={activeTab === 1 ?{...styles.tab,...styles.tabActive,...(hovered==1 && styles.tabHover)} : {...styles.tab,...(hovered==1 && styles.tabHover)}}
+        {(radialVal || conicVal) && <div
+          onMouseEnter={() => setHovered(1)}
+          onMouseDown={() => setHovered(false)}
+          onMouseLeave={() => setHovered(false)}
+          style={activeTab === 1 ? { ...styles.tab, ...styles.tabActive, ...(hovered == 1 && styles.tabHover) } : { ...styles.tab, ...(hovered == 1 && styles.tabHover) }}
           onClick={() => handleTabClick(1)}
         >
           Linear
         </div>}
-     {radialVal &&  <div
-        onMouseEnter={() => setHovered(2)}
-        onMouseDown={() => setHovered(false)}
-        onMouseLeave={() => setHovered(false)}
-        style={activeTab === 2 ?{...styles.tab,...styles.tabActive,...(hovered==2 && styles.tabHover)} : {...styles.tab,...(hovered==2 && styles.tabHover)}}
+        {radialVal && <div
+          onMouseEnter={() => setHovered(2)}
+          onMouseDown={() => setHovered(false)}
+          onMouseLeave={() => setHovered(false)}
+          style={activeTab === 2 ? { ...styles.tab, ...styles.tabActive, ...(hovered == 2 && styles.tabHover) } : { ...styles.tab, ...(hovered == 2 && styles.tabHover) }}
           onClick={() => handleTabClick(2)}
         >
           Radial
         </div>}
-     {conicVal &&   <div
-        onMouseEnter={() => setHovered(3)}
-        onMouseDown={() => setHovered(false)}
-        onMouseLeave={() => setHovered(false)}
-        style={activeTab === 3 ?{...styles.tab,...styles.tabActive,...(hovered==3 && styles.tabHover)} : {...styles.tab,...(hovered==3 && styles.tabHover)}}
+        {conicVal && <div
+          onMouseEnter={() => setHovered(3)}
+          onMouseDown={() => setHovered(false)}
+          onMouseLeave={() => setHovered(false)}
+          style={activeTab === 3 ? { ...styles.tab, ...styles.tabActive, ...(hovered == 3 && styles.tabHover) } : { ...styles.tab, ...(hovered == 3 && styles.tabHover) }}
           onClick={() => handleTabClick(3)}
         >
           Conic
@@ -221,71 +225,62 @@ useEffect(()=>{
 
       <div style={styles.tabcontent}>
         {/* Content for Tab 1 */}
-          <div style={styles.gradientSection}>
+        <div style={styles.gradientSection}>
 
-            <div style={(!radialVal && !conicVal && showCloseBtn) ?{paddingTop:'15px',...styles.inputSection} :styles.inputSection} >
-              <div style={{...styles.gradientSectionInputDiv,height: '25px',
-      width: '25px',
-      borderRadius: '6px',
-      cursor: 'pointer'}}>
-              {colorOne.map((e,i)=>{
-                return (<div style={selectedInput===i+1 ? {height: '25px',
-                width: '25px',
-                borderRadius: '6px',
-                cursor: 'pointer',border:'2px solid '+e}:{}} key={i} >
-                <div  style={selectedInput!==i+1 ? {height: '25px',
-                width: '25px',
-                borderRadius: '6px',
-                cursor: 'pointer',border:'2px solid '+e,background: e,...styles.inputSectionDiv}:{height: '25px',
-                width: '25px',
-                borderRadius: '6px',
-                cursor: 'pointer',border:'2px solid '+e,background: e, margin:'3px'}} onClick={()=>{setSelected(e);setSelectedInput(i+1)}}></div></div>)
+          <div style={(!radialVal && !conicVal && showCloseBtn) ? { paddingTop: '15px', ...styles.inputSection } : styles.inputSection} >
+            <div style={{ ...styles.gradientSectionInputDiv, ...styles.outerDivStyle }}>
+              {colorOne.map((e, i) => {
+                return (<div style={selectedInput === i + 1 ? { ...styles.outerDivStyle, border: '2px solid ' + e } : {}} key={i} >
+                  <div style={selectedInput !== i + 1 ? { ...styles.outerDivStyle, border: '2px solid ' + e, background: e, ...styles.inputSectionDiv } : { ...styles.outerDivStyle, border: '2px solid ' + e, background: e, margin: '3px' }} onClick={() => { setSelected(e); setSelectedInput(i + 1) }}></div></div>)
               })}
-              {colorOne.length < 4 &&<span><HiPlusCircle  style={{...styles.inputSectionSpanSvg,...styles.inputSectionDiv}} onClick={()=> {setColorOne((current => [... current, '#000000'])); setTimeout(() => {
-                setSelectedInput(colorOne.length+1)
-                setSelected('#000000')
-              }, 50)
-              }}/></span>}
-              </div>
-              {colorOne.length > 2 && <RiDeleteBin5Fill style={styles.deleteIcon} onClick={deleteColor}/>}
-
+              {colorOne.length < 4 && <span><HiPlusCircle style={{ ...styles.inputSectionSpanSvg, ...styles.inputSectionDiv }} onClick={() => {
+                setColorOne((current => [...current, '#000000'])); setTimeout(() => {
+                  setSelectedInput(colorOne.length + 1)
+                  setSelected('#000000')
+                }, 50)
+              }} /></span>}
             </div>
-
-            <div style={styles.pickerSetting}>
-                <ColorPicker setColor={selected} selectedColor={selectedColor} style={styles.gradientSectionSvg}/>
-
-            {activeTab===1 && <div style={styles.direction}>
-            <GrPowerReset onClick={()=>setDirection('')} style={direction==='' ? styles.selectedSvg : styles.gradientSectionSvg} />
-            <CgArrowTopRight onClick={()=>setDirection('to right top')} style={direction==='to right top' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowRight onClick={()=>setDirection('to right')} style={direction==='to right' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowBottomRight onClick={()=>setDirection('to right bottom')} style={direction==='to right bottom' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowDown onClick={()=>setDirection('to bottom')} style={direction==='to bottom' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowBottomLeft onClick={()=>setDirection('to left bottom')} style={direction==='to left bottom' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowLeft onClick={()=>setDirection('to left')} style={direction==='to left' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowTopLeft onClick={()=>setDirection('to left top')} style={direction==='to left top' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowUp onClick={()=>setDirection('to top')} style={direction==='to top' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            </div>}
-            {activeTab===2 && <div style={styles.direction}>
-
-              <GrPowerReset onClick={()=>setDirection('')} style={direction==='' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-              <GoDotFill onClick={()=>setDirection('circle')} style={direction==='circle' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-              </div>}
-              {activeTab===3 && <div style={styles.direction}>
-
-<GrPowerReset onClick={()=>setDirection('')} style={direction==='' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-<CgArrowTopRight onClick={()=>setDirection('from 45deg')} style={direction==='from 45deg' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowRight onClick={()=>setDirection('from 90deg')} style={direction==='from 90deg' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowBottomRight onClick={()=>setDirection('from 135deg')} style={direction==='from 135deg' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowDown onClick={()=>setDirection('from 180deg')} style={direction==='from 180deg' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowBottomLeft onClick={()=>setDirection('from 225deg')} style={direction==='from 225deg' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowLeft onClick={()=>setDirection('from 270deg')} style={direction==='from 270deg' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowTopLeft onClick={()=>setDirection('from 315deg')} style={direction==='from 315deg' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-            <CgArrowUp onClick={()=>setDirection('from 360deg')} style={direction==='from 360deg' ? styles.selectedSvg : styles.gradientSectionSvg}/>
-</div>}
-            </div>
-            <p style={styles.tabcontentP}>{output}</p>
+            {colorOne.length > 2 && <RiDeleteBin5Fill style={styles.deleteIcon} onClick={deleteColor} />}
 
           </div>
+
+          <div style={styles.pickerSetting}>
+            <ColorPicker setColor={selected} selectedColor={selectedColor} style={styles.gradientSectionSvg} />
+
+            {activeTab === 1 && <div style={styles.direction}>
+              <GrPowerReset onClick={() => setDirection('')} style={direction === '' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowTopRight onClick={() => setDirection('to right top')} style={direction === 'to right top' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowRight onClick={() => setDirection('to right')} style={direction === 'to right' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowBottomRight onClick={() => setDirection('to right bottom')} style={direction === 'to right bottom' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowDown onClick={() => setDirection('to bottom')} style={direction === 'to bottom' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowBottomLeft onClick={() => setDirection('to left bottom')} style={direction === 'to left bottom' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowLeft onClick={() => setDirection('to left')} style={direction === 'to left' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowTopLeft onClick={() => setDirection('to left top')} style={direction === 'to left top' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowUp onClick={() => setDirection('to top')} style={direction === 'to top' ? styles.selectedSvg : styles.gradientSectionSvg} />
+            </div>}
+
+            {activeTab === 2 && <div style={styles.direction}>
+
+              <GrPowerReset onClick={() => setDirection('')} style={direction === '' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <GoDotFill onClick={() => setDirection('circle')} style={direction === 'circle' ? styles.selectedSvg : styles.gradientSectionSvg} />
+            </div>}
+
+            {activeTab === 3 && <div style={styles.direction}>
+
+              <GrPowerReset onClick={() => setDirection('')} style={direction === '' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowTopRight onClick={() => setDirection('from 45deg')} style={direction === 'from 45deg' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowRight onClick={() => setDirection('from 90deg')} style={direction === 'from 90deg' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowBottomRight onClick={() => setDirection('from 135deg')} style={direction === 'from 135deg' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowDown onClick={() => setDirection('from 180deg')} style={direction === 'from 180deg' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowBottomLeft onClick={() => setDirection('from 225deg')} style={direction === 'from 225deg' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowLeft onClick={() => setDirection('from 270deg')} style={direction === 'from 270deg' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowTopLeft onClick={() => setDirection('from 315deg')} style={direction === 'from 315deg' ? styles.selectedSvg : styles.gradientSectionSvg} />
+              <CgArrowUp onClick={() => setDirection('from 360deg')} style={direction === 'from 360deg' ? styles.selectedSvg : styles.gradientSectionSvg} />
+            </div>}
+          </div>
+          <p style={styles.tabcontentP}>{output}</p>
+
+        </div>
       </div>
     </div>
   );
